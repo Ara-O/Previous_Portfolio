e78e01b4a80cb1d18ea9b8e7ae07be4f9d045312
Ara <oladipoeyiara@gmail.com>
2021-04-17 22:29:03 -0400

    Smoothened animations and tweaked navbar

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const navbar = document.querySelector(".verticalnavbar");
 const navbaricon = document.querySelector(".hamburgericon");
+const body = document.querySelector("body");
+const downArrow = document.querySelector(".downarrow");
+const navlinks = document.querySelector(".links");
+const navlinksul = document.querySelector(".linksul");
 
 navbaricon.addEventListener("click", () => {
   navbar.classList.toggle("slide");
+});
+
+downArrow.addEventListener("click", function (e) {
+  document.querySelector(e.target.dataset.scrollto).scrollIntoView({
+    behavior: "smooth",
+  });
+});
+
+navlinksul.addEventListener("click", function (e) {
+  const clicked = e.target;
+  if (clicked.tagName === "LI") {
+    console.log(clicked);
+    document.querySelector(clicked.dataset.scrollto).scrollIntoView({
+      behavior: "smooth",
+    });
+  }
 });
